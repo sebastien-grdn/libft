2e2d538033278c6bb21a24679eb7bfd6fce2c7db
sg9031 <sg9031@gmail.com>
2020-12-20 17:38:07 +0100

    new functions + .h file

--- Makefile
+++ Makefile
@@ -6,12 +6,13 @@
 #    By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/20 13:26:20 by sg9031            #+#    #+#              #
-#    Updated: 2020/12/20 13:40:26 by sg9031           ###   ########.fr        #
+#    Updated: 2020/12/20 17:33:13 by sg9031           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS	= ft_tolower.c ft_toupper.c
-OBJS	= $(addprefix ${DIR}, ${SRCS:.c=.o})
+SRCS	= ft_tolower.c ft_toupper.c ft_strchr.c ft_strrchr.c ft_strdup.c ft_isalpha.c ft_strlen.c \
+		  ft_isalnum.c ft_isascii.c ft_isprint.c ft_isdigit.c ft_strncmp.c ft_strlcat.c
+OBJS	= ${SRCS:.c=.o}
 CC		= gcc
 CFLAGS	= -Wall -Wextra -Werror
 NAME	= libft.a
---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/20 17:32:58 by sg9031            #+#    #+#             */
+/*   Updated: 2020/12/20 17:36:19 by sg9031           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	const int		dest_len = ft_strlen(dest);
+	const int		src_len = ft_strlen(src);
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	if (size <= i)
+		return (size + src_len);
+	while (i < size - 1 && src[j])
+		dest[i++] = src[j++];
+	if (size > 0)
+		dest[i] = '\0';
+	while (src[j])
+		j++;
+	return (src_len + dest_len);
+}
