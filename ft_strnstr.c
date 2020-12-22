56acc2d3e5e036fa4c9e6ba351745cf3a5b84ac7
sg9031 <sg9031@gmail.com>
2020-12-22 16:30:18 +0100

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 19:20:00 by sg9031            #+#    #+#             */
+/*   Updated: 2020/12/22 16:29:15 by sg9031           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libft.h"
+
+char* ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t nlen;
+
+    nlen = ft_strlen(needle);
+    if (nlen == 0)
+        return ((char *)haystack);
+    if (!needle)
+        return ((char *)haystack);
+    while (*haystack && len >= nlen)
+    {
+        if (ft_strncmp((char *)needle, (char *)haystack, nlen) == 0)
+            return ((char *)haystack);
+        haystack++;
+        len--;
+    }
+    return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 17:36:23 by sg9031            #+#    #+#             */
-/*   Updated: 2020/12/21 19:12:16 by sg9031           ###   ########.fr       */
+/*   Updated: 2020/12/21 21:03:13 by sg9031           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 int	ft_strlen(const char *str);
 void* ft_memset(void *b, int c, size_t len);
 void* ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 #endif
