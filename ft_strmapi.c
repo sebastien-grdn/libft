62a170d8c64e80863b130f380270cf9ecf7111b1
sg9031 <sg9031@gmail.com>
2020-12-23 20:45:44 +0100

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 19:01:13 by sg9031            #+#    #+#             */
+/*   Updated: 2020/12/23 20:45:09 by sg9031           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char* new;
+    int len;
+    int i;
+
+    if (!s || !f)
+        return (NULL);
+    len = ft_strlen(s);
+    if (!(new = malloc((len + 1) * sizeof(char))))
+        return (NULL);
+    i = -1;
+    while (++i < len)
+        new[i] = f(i, s[i]);
+    new[len] = '\0';
+    return (new);
+}
