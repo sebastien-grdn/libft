21bd43c223afbba0928789bc234f6079c8873750
sg9031 <sg9031@gmail.com>
2021-01-02 22:29:02 +0100

    repair memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 15:40:40 by sg9031            #+#    #+#             */
-/*   Updated: 2020/12/24 19:26:38 by sg9031           ###   ########.fr       */
+/*   Updated: 2021/01/02 22:26:48 by sg9031           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	cs1 = (unsigned char*)s1 - 1;
 	cs2 = (unsigned char*)s2 - 1;
+	if (!n)
+		return (0);
 	while (*++cs1 == *++cs2 && --n)
 		;
 	return (*cs1 - *cs2);
