2fe1e199432dd134a27cbb8c986278f3461ae21d
sg9031 <sg9031@gmail.com>
2021-01-11 23:02:20 +0100

    forgot lib

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,13 +6,11 @@
 /*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 11:43:30 by sg9031            #+#    #+#             */
-/*   Updated: 2021/01/11 22:57:50 by sg9031           ###   ########.fr       */
+/*   Updated: 2021/01/11 23:01:51 by sg9031           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-
 
 void	*ft_memccpy(
 	void *dst, const void *src, int c, size_t n
