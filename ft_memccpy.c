493d0b9ed6b496bd33d05722e42d018253c680b7
sg9031 <sg9031@gmail.com>
2021-01-11 23:00:00 +0100

    fix memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,11 +6,13 @@
 /*   By: sg9031 <sg9031@gmail.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 11:43:30 by sg9031            #+#    #+#             */
-/*   Updated: 2020/12/27 15:17:01 by sg9031           ###   ########.fr       */
+/*   Updated: 2021/01/11 22:57:50 by sg9031           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
 
 void	*ft_memccpy(
 	void *dst, const void *src, int c, size_t n
@@ -20,19 +22,16 @@ void	*ft_memccpy(
 	unsigned char	*x;
 	unsigned char	*y;
 
-	if (!dst && !src)
-		return (dst);
 	x = (unsigned char *)src;
 	y = (unsigned char *)dst;
-	i = -1;
-	while (++i < n && x[i] != (unsigned char)c)
+	i = 0;
+	while (i < n && x[i] != (unsigned char)c)
 	{
 		y[i] = (unsigned char)x[i];
+		i++;
 	}
-	if (x[i] == (unsigned char)c)
-	{
-		y[i] = (unsigned char)x[i];
-		return (&dst[i + 1]);
-	}
-	return (NULL);
+	if (i == n)
+		return (NULL);
+	y[i] = (unsigned char)c;
+	return (&dst[i + 1]);
 }
